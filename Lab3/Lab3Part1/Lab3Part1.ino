d71c432e48f5360865ef3c7bb7aaee8889d1dece
Shahnaz Mohideen <smohid@uw.edu>
2025-02-10 18:02:09 -0800

    lab3 part2 working

--- Lab3/Lab3Part1/Lab3Part1.ino
+++ Lab3/Lab3Part1/Lab3Part1.ino
@@ -1,42 +1,30 @@
+/*
+Filename: Lab3Part1
+Authors: Shahnaz Mohideen, Alison Tea
+Date: 02/20/2025
+Description: This module takes in a string input in the serial moniter and prints
+it out on the lcd display using the Wire library
+*/
+
+// Includes
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+// Global Variable
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Initialize the LCD
+
+// Function Prototypes
+void sendCommand(uint8_t);
+void sendData(uint8_t);
 
 void setup() {
   Serial.begin(115200);
-
+  
   Wire.begin(11,12);
   lcd.init();
   lcd.backlight();
 
   delay(2);
-}
-
-void sendCommand(uint8_t command) {
-    Wire.beginTransmission(0x27);
-    Wire.write((command & 0xF0) | 0x08); // Send high nibble with backlight on
-    Wire.write((command & 0xF0) | 0x0C); // Enable bit high
-    Wire.write((command & 0xF0) | 0x08); // Enable bit low
-    
-    Wire.write((command << 4) | 0x08); // Send low nibble with backlight on
-    Wire.write((command << 4) | 0x0C); // Enable bit high
-    Wire.write((command << 4) | 0x08); // Enable bit low
-    Wire.endTransmission();
-    delay(2);
-}
-
-void sendData(uint8_t data) {
-    Wire.beginTransmission(0x27);
-    Wire.write((data & 0xF0) | 0x09); // Send high nibble with RS=1, Enable=0
-    Wire.write((data & 0xF0) | 0x0D); // Enable bit high
-    Wire.write((data & 0xF0) | 0x09); // Enable bit low
-    
-    Wire.write((data << 4) | 0x09); // Send low nibble with RS=1, Enable=0
-    Wire.write((data << 4) | 0x0D); // Enable bit high
-    Wire.write((data << 4) | 0x09); // Enable bit low
-    Wire.endTransmission();
-    delay(2);
 }
 
 void loop() {
@@ -52,4 +40,35 @@ void loop() {
             }
         }
     }
+}
+
+// Name: sendCommand
+// Description: Takes a command and sends it to the lcd uisng the 
+// Wire library
+void sendCommand(uint8_t command) {
+    Wire.beginTransmission(0x27);
+    Wire.write((command & 0xF0) | 0x08); // Send high nibble with backlight on
+    Wire.write((command & 0xF0) | 0x0C); // Enable bit high
+    Wire.write((command & 0xF0) | 0x08); // Enable bit low
+    
+    Wire.write((command << 4) | 0x08); // Send low nibble with backlight on
+    Wire.write((command << 4) | 0x0C); // Enable bit high
+    Wire.write((command << 4) | 0x08); // Enable bit low
+    Wire.endTransmission();
+    delay(2);
+}
+
+// Name: sendData
+// Description: Takes in a char and writes it to the lcd display
+void sendData(uint8_t data) {
+    Wire.beginTransmission(0x27);
+    Wire.write((data & 0xF0) | 0x09); // Send high nibble with RS=1, Enable=0
+    Wire.write((data & 0xF0) | 0x0D); // Enable bit high
+    Wire.write((data & 0xF0) | 0x09); // Enable bit low
+    
+    Wire.write((data << 4) | 0x09); // Send low nibble with RS=1, Enable=0
+    Wire.write((data << 4) | 0x0D); // Enable bit high
+    Wire.write((data << 4) | 0x09); // Enable bit low
+    Wire.endTransmission();
+    delay(2);
 }
---
+++ Lab3/Lab3Part2/Lab3Part2.ino
@@ -0,0 +1,199 @@
+// Includes
+#include <Arduino.h>
+#include <driver/ledc.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define LED_PIN 5   // External LED pin
+#define BUZZER_PIN 12 // Buzzer pin for music notes
+#define LEDC_CHANNEL 0
+#define LEDC_FREQ 1000
+#define LEDC_RESOLUTION 8
+#define NUM_TASKS 4
+#define MAX_TASKS 5
+// LCD setup
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Global timers
+static int taskA_timer = 0;
+static int taskB_timer = 0;
+static int taskC_timer = 0;
+static int taskD_timer = 0;
+
+// Function prototypes
+void taskA();
+void taskB();
+void taskC();
+void taskD();
+
+// TCB struct
+struct TCB {
+ void (*taskFunction)();  // Pointer to the task function
+ bool isRunning;          // State of the task
+ bool isDone;             // Whether the task is completed or not
+ int pid;                 // Unique process ID
+ int priority;	      // The priority level of the task
+};
+
+// Array to store all the task control blocks (TCBs)
+TCB TaskList [MAX_TASKS];
+
+// function pointer typedef
+typedef void (*funcPtr)();
+
+funcPtr taskA_Ptr = taskA;
+funcPtr taskB_Ptr = taskB;
+funcPtr taskC_Ptr = taskC;
+funcPtr taskD_Ptr = taskD;
+
+
+// Notes frequency array (example melody)
+int melody[] = {262, 294, 330, 349, 392, 440, 494, 523, 587, 659}; 
+
+void setup() {
+    Serial.begin(115200);
+    while(!Serial);
+
+    pinMode(LED_PIN, OUTPUT);
+
+    Wire.begin(8,9);
+
+    lcd.init();
+    lcd.backlight();
+    delay(2);
+    
+    ledcAttach(BUZZER_PIN, LEDC_FREQ, LEDC_RESOLUTION);
+
+    // setup TCB list
+
+    int j = 0;
+
+    TaskList[j].taskFunction = taskA_Ptr;
+    TaskList[j].isRunning = false;
+    TaskList[j].isDone = false;
+    TaskList[j].pid = j;
+    TaskList[j].priority = 4;
+    j++;
+
+    TaskList[j].taskFunction = taskB_Ptr;
+    TaskList[j].isRunning = false;
+    TaskList[j].isDone = false;
+    TaskList[j].pid = j;
+    TaskList[j].priority = 3;
+    j++;
+
+    TaskList[j].taskFunction = taskC_Ptr;
+    TaskList[j].isRunning = false;
+    TaskList[j].isDone = false;
+    TaskList[j].pid = j;
+    TaskList[j].priority = 2;
+    j++;
+
+    TaskList[j].taskFunction = taskD_Ptr;
+    TaskList[j].isRunning = false;
+    TaskList[j].isDone = false;
+    TaskList[j].pid = j;
+    TaskList[j].priority = 1;
+    j++;
+
+    TaskList[j].taskFunction = NULL;
+
+    
+}
+
+
+void loop() {
+  sortTasks();
+  for(int i = 0; i < MAX_TASKS; i++){
+    if(TaskList[i].taskFunction != NULL && TaskList[i].isRunning == false && TaskList[i].isDone == false){
+      TaskList[i].isRunning = true;
+      TaskList[i].taskFunction(); // this executes the task by calling the function using the function pointer
+      TaskList[i].isRunning = false;
+      TaskList[i].isDone = true;
+      Serial.print(TaskList[i].priority);
+      Serial.println();
+    }else{
+      break;
+    }
+  }
+
+  delay(10);
+
+  for(int i = 0; i < NUM_TASKS; i++){
+    TaskList[i].priority = TaskList[i].priority % 4 + 1;
+    TaskList[i].isDone = false;
+  }
+}
+
+// Name: sortTasks
+// Descripion: sort tasks by priority using bubble sort
+void sortTasks() {
+    for (int i = 0; i < NUM_TASKS - 1; i++) {
+        for (int j = 0; j < NUM_TASKS - i - 1; j++) {
+            if (TaskList[j].priority < TaskList[j + 1].priority) {
+                TCB temp = TaskList[j];
+                TaskList[j] = TaskList[j + 1];
+                TaskList[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Description: Turns an external LED on and off eight times 
+// in one-second intervals.
+void taskA() {
+    
+   for(taskA_timer = 1; taskA_timer <= 8; taskA_timer++){
+        digitalWrite(LED_PIN, HIGH);
+        delay(500);
+        digitalWrite(LED_PIN, LOW);
+        delay(500);
+        taskA_timer++;
+  }
+
+  Serial.print("LED Blinker: ");
+}
+
+// Description: Counts up from 1 to 10 on LCD.
+void taskB() {
+    lcd.clear();
+    for (taskB_timer = 1; taskB_timer <= 10; taskB_timer++) {
+        lcd.setCursor(0, 0);
+        lcd.print("Count: ");
+        lcd.print(taskB_timer);
+        delay(1000);
+    }
+
+    Serial.print("Counter: ");
+}
+
+// Description: Plays a melody and displays the note voltage levels.
+void taskC() {
+    ledcAttach(BUZZER_PIN, LEDC_FREQ, LEDC_RESOLUTION);
+
+    for (taskC_timer = 0; taskC_timer < 10; taskC_timer++) {
+        int freq = melody[taskC_timer];
+
+        // Output frequency using LEDC
+        ledcWrite(BUZZER_PIN, freq);
+        Serial.print("Playing note: ");
+        Serial.println(freq);
+
+        delay(500);
+    }
+    ledcWrite(BUZZER_PIN, 0); // Stop tone
+
+    Serial.print("Music Player: ");
+}
+
+
+// Description: Prints A-Z to Serial Monitor.
+void taskD() {
+    for (char letter = 'A'; letter <= 'Z'; letter++) {
+        Serial.print(letter);
+        Serial.print(" ");
+        delay(250);
+    }
+    Serial.println();
+    Serial.print("Alphabet Printer: ");
+}
