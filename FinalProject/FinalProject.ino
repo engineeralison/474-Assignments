77361375b5583004157ed87a57f69683e542a123
smohid26 <129727189+smohid26@users.noreply.github.com>
2025-03-10 17:16:21 -0700

    add temp and hum to queue

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -13,7 +13,9 @@ Adafruit_AM2320 am2320 = Adafruit_AM2320();
 // Pin Definitions
 #define MOTION_SENSOR_PIN 16
 
-#define MOTION 1
+#define MOTION 0
+#define TEMP 1
+#define HUM 2
 
 // Queue and Semaphore
 QueueHandle_t sensorQueue;
@@ -22,6 +24,7 @@ SemaphoreHandle_t queueSemaphore;
 TaskHandle_t Motion_Handle;
 TaskHandle_t SerialMoniter_Handle;
 TaskHandle_t TemperatureHumidity_Handle;
+TaskHandle_t Sound_Handle;
 
 // Sensor Data Structure
 typedef struct {
@@ -55,14 +58,18 @@ void Task_TemperatureHumidity(void *pvParameters) {
     while (1) {
         
         if (xSemaphoreTake(queueSemaphore, portMAX_DELAY) == pdTRUE) {
-            if (uxQueueSpacesAvailable(sensorQueue) > 0) {
-              Serial.print("Temp: ");
-              Serial.print(am2320.readTemperature());
-              Serial.print(" C\t");
+            if (uxQueueSpacesAvailable(sensorQueue) > 1) {
+              SensorData data1 = {TEMP, am2320.readTemperature()};
+              xQueueSend(sensorQueue, &data1, portMAX_DELAY);
+              SensorData data2 = {HUM, am2320.readHumidity()};
+              xQueueSend(sensorQueue, &data2, portMAX_DELAY);
+              //Serial0.print("Temp: ");
+              //Serial0.print(am2320.readTemperature());
+              //Serial0.print(" C\t");
 
-              Serial.print("Humidity: ");
-              Serial.print(am2320.readHumidity());
-              Serial.println(" %");
+              //Serial0.print("Humidity: ");
+              //Serial0.print(am2320.readHumidity());
+              //Serial0.println(" %");
             }
             xSemaphoreGive(queueSemaphore);
         }
@@ -70,6 +77,8 @@ void Task_TemperatureHumidity(void *pvParameters) {
         vTaskDelay(pdMS_TO_TICKS(500));  // Check every 500ms
     }
 }
+
+
 
 // Serial Monitor Display Task (Consumer)
 void Task_SerialMonitor(void *pvParameters) {
@@ -79,7 +88,11 @@ void Task_SerialMonitor(void *pvParameters) {
           if(uxQueueSpacesAvailable(sensorQueue) < 10){
             if (xQueueReceive(sensorQueue, &receivedData, portMAX_DELAY) == pdTRUE) {
                 if(receivedData.sensor == MOTION){
-                Serial.printf("Motion: %d\n", receivedData.data);
+                  Serial0.printf("Motion: %d\n", receivedData.data);
+                } else if (receivedData.sensor == TEMP){
+                  Serial0.printf("Temperature: %d\n", receivedData.data);
+                } else if (receivedData.sensor == HUM){
+                  Serial0.printf("Humidity: %d\n", receivedData.data);
                 }
             }
           }
@@ -89,14 +102,13 @@ void Task_SerialMonitor(void *pvParameters) {
 }
 
 void setup() {
-    Serial.begin(115200);
-    while(!Serial);
-    Serial.println("starting");
+    Serial0.begin(115200);
+    Serial0.println("starting");
 
     // Create Queue (size: 10 elements)
     sensorQueue = xQueueCreate(10, sizeof(SensorData));
     if (sensorQueue == NULL) {
-        Serial.println("Error creating queue!");
+        Serial0.println("Error creating queue!");
         while (1);
     }
 
@@ -104,10 +116,10 @@ void setup() {
     Wire.begin(8,9);
 
     if (!am2320.begin()) {
-    Serial.println("Failed to detect AM2320 sensor! Check wiring.");
+    Serial0.println("Failed to detect AM2320 sensor! Check wiring.");
     while (1) { delay(10); } // Stop execution if sensor isn't found
     }
-    Serial.println("AM2320 sensor found!");
+    Serial0.println("AM2320 sensor found!");
     am2320.begin();
 
     // Create Semaphore
@@ -117,6 +129,8 @@ void setup() {
     // Create Producer Tasks (reading from sensor)
     xTaskCreatePinnedToCore(Task_Motion, "Motion Task", 2048, NULL, 1, &Motion_Handle, 0);
     xTaskCreatePinnedToCore(Task_TemperatureHumidity, "Temperature and Humidity Task", 2048, NULL, 1, &TemperatureHumidity_Handle, 0);
+    xTaskCreatePinnedToCore(Task_Sound, "Sound Task", 2048, NULL, 1, &Sound_Handle, 0);
+
     // Create Consumer Tasks (process sensor data)
     xTaskCreatePinnedToCore(Task_SerialMonitor, "Serial Task", 2048, NULL, 1, &SerialMoniter_Handle, 0);
 }
