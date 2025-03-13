2fbefce3eb7e0e48a8245216800ec3d59de73fbc
engineeralison <155679486+engineeralison@users.noreply.github.com>
2025-03-12 18:47:42 -0700

    Update Reciever.ino

--- FinalProject/Reciever/Reciever.ino
+++ FinalProject/Reciever/Reciever.ino
@@ -9,35 +9,44 @@
 #define SOUND 3
 #define FLOOD 4
 
-
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-volatile bool messageReceived = false;
-volatile uint8_t sensor = 0;
+ volatile bool messageReceived = false;
+ volatile int sensorMessage;
 
-
+// Name: dataRecieved
+// Description: This callback function will be invoked when signal is received
+// 			        over ESP-NOW. Implement the necessary functionality that will 
+//			        trigger the message to the LCD.
+//
 void IRAM_ATTR dataReceived(const esp_now_recv_info_t * esp_now_info, const uint8_t *incomingData, int len)
 {
-// =========> TODO: This callback function will be invoked when signal is received
-// 			over ESP-NOW. Implement the necessary functionality that will 
-//			trigger the message to the LCD.
-//
-  messageReceived = true;
-  sensor = *incomingData;
-}
 
+  if (len >= sizeof(sensorMessage)) len = sizeof(sensorMessage) - 1; // Prevent overflow
+  
+  messageReceived = true;
+  memcpy((int*) &sensorMessage,incomingData, len);
+  // sensorMessage[len] = '\0';
+
+  Serial.printf("Data: %d\n",  incomingData );
+  
+}
 
 void setup() {
 
- Serial0.begin(115200);
- Serial0.println("Starting\n");
+ Serial.begin(115200);
+ while(!Serial);
+
+
+ Serial.println("Starting\n");
  WiFi.mode(WIFI_STA);
-// =========> TODO: Initialize LCD display
-Wire.begin(8,9);
-lcd.init();
-lcd.backlight();   // Ensure backlight is ON
-lcd.setCursor(0, 0);
+ // Initialize LCD display
+ Wire.begin(8,9);
+ lcd.init();
+ lcd.backlight();   // Ensure backlight is ON
+ lcd.setCursor(0, 0);
 
-
+  // Write welcome message to the alarm
+  lcd.print("You are safe.");
  // Initializes ESP-NOW and check if it was successful; if not, exit the setup function
  if (esp_now_init() != ESP_OK) return;
  // Registers the callback function 'dataReceived' to be called when data is received   
@@ -45,29 +54,24 @@ lcd.setCursor(0, 0);
  esp_now_register_recv_cb(dataReceived);
 }
 
-
 void loop() {
-// =========> TODO: Print out an incrementing counter to the LCD.
-//			If a signal has been received over ESP-NOW, print out “New
-// 			Message!” on the LCD
-
-  if(messageReceived){
+  */
+   if(messageReceived){
     lcd.clear();
     lcd.setCursor(0, 0);
-    if(sensor == MOTION){
-      lcd.print("Motion");
-    }else if (sensor = TEMP){
-      lcd.print("Temperature");
-    }else if (sensor = HUM){
-      lcd.print("Humidity");
-    }else if (sensor == SOUND){
-      lcd.print("Sound");
-    }else if (sensor == FLOOD){
-      lcd.print("Water");
+    Serial.printf("Sensor Message: %d\n", sensorMessage);
+    if(sensorMessage == MOTION){
+      lcd.print("BREAK-IN! HIDE!");
+    }else if (sensorMessage == HUM){
+      lcd.print("FIRE! RUN!");
+    }else if (sensorMessage == SOUND){
+      lcd.print("BREAK-IN! HIDE!"");
+    }else if (sensorMessage == FLOOD){
+      lcd.print("FLOOD! EVACUATE!");
     }
-    delay(2000);
+    // delay(2000);
     messageReceived = false;
   }
 
-  
+
 }
